e8cdd1b9d2a5892b91b22794552c2222eb1af755
Mada Gower <gmgower@gmail.com>
2020-05-27 20:40:21 -0500

    contains BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,12 +17,14 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
+        # self wil be the root
         # compare value to the current node 
       
 
         # if no node to go to, (either left or right)
             # make the new node at that spot
         while True:
+        # compare value to the current node 
         # if smaller, go left
             if value < self.value:
                 if self.left is None:
@@ -41,7 +43,23 @@ class BSTNode:
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        while True:
+            # if equal return True
+            if self.value == target:
+                return True
+            # check right
+            if target > self.value:
+                if self.right is None:
+                    return False
+                else:
+                    self = self.right
+            # check left
+            elif target < self.value:
+                if self.left is None:
+                    return False
+                else:
+                    self = self.left
+            
 
     # Return the maximum value found in the tree
     def get_max(self):
